038b8a0570d7fda14eb01c302bc640cca92260eb
Powerrice1 <thenguyenshow@gmail.com>
2019-03-24 13:40:06 -0500

    Animation, Materials, and Mesh

--- GameArt/Assets/Scripts/Enemy/Action/Enemy_movement.cs
+++ GameArt/Assets/Scripts/Enemy/Action/Enemy_movement.cs
@@ -5,35 +5,71 @@ using UnityEngine;
 
 public class Enemy_movement : MonoBehaviour {
 
+    public Transform[] points;
+    private int destPoint = 0;
+    private NavMeshAgent agent;
     [SerializeField]
-    Transform Destination1, Destination2;
+    float waitTime;
+    float timer;
 
-    NavMeshAgent navMeshAgent;
-    
+    private Melee_enemy_Sight sight;
 
-	// Use this for initialization
-	void Start () {
-        navMeshAgent = this.GetComponent<NavMeshAgent>();
-
-        if(navMeshAgent == null)
-        {
-            Debug.Log("WHERE IS IT????");
-        }
-        else
-        {
-            SetDestination();
-        }
-	}
-	
-	// Update is called once per frame
-	void SetDestination()
+    void Start()
     {
-        if(Destination1 != null)
+        timer = waitTime;
+        agent = GetComponent<NavMeshAgent>();
+        sight = GetComponentInChildren<Melee_enemy_Sight>();
+
+        // Disabling auto-braking allows for continuous movement
+        // between points (ie, the agent doesn't slow down as it
+        // approaches a destination point).
+        //agent.autoBraking = false;
+
+        GotoNextPoint();
+    }
+
+
+    void GotoNextPoint()
+    {
+        // Returns if no points have been set up
+        if (points.Length == 0)
+            return;
+
+        // Set the agent to go to the currently selected destination.
+
+        agent.destination = points[destPoint].position;
+
+
+        // Choose the next point in the array as the destination,
+        // cycling to the start if necessary.
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
+
+
+    void Update()
+    {
+        // Choose the next destination point when the agent gets
+        // close to the current one.
+        if (!agent.pathPending && agent.remainingDistance < 0.25f)
         {
-            Vector3 target = Destination1.transform.position;
-            navMeshAgent.SetDestination(target);
+            //On_Off();
+            GotoNextPoint();
         }
     }
 
+    void On_Off()
+    {
+        agent.Stop();
+        timer -= Time.deltaTime;
+        Debug.Log(timer);
+        if(timer <= 0)
+        {
+            Debug.Log("Resume");
+            agent.Resume();
+            timer = waitTime;
+        }
+
+    }
 
 }
