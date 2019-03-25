b32b19f4f99e815379d4d2bb6686fbd6d24e873a
makutajoe <43686893+makutajoe@users.noreply.github.com>
2019-03-25 07:21:22 -0500

    Checkpoint Scripts
    Checkpoint Scripts

---
+++ GameArt/Assets/Scripts/Game Manager/SaveCheckpoint/CheckpointManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CheckpointManager : MonoBehaviour
+{
+    public bool checkpointSaved = false;
+    public Scene checkpointScene;
+    public Vector3 checkpointLocation;
+    private GameObject player;
+
+    void Start()
+    {
+        player = GameObject.Find("Player(Cube)");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Slash) && player != null && checkpointSaved)
+        {
+            //Destroy(player);
+            //Insert code for loading scene first, if scene is different
+            var clone = Instantiate(player, checkpointLocation, player.transform.rotation);
+            clone.transform.localScale = new Vector3(1, 1, 1);
+            clone.name = "Player(Cube)";
+            Destroy(player);
+            player = clone;
+        }
+    }
+}
