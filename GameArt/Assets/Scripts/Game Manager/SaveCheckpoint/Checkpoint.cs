f0c381f2d86a939d3c6c461621a23f6ea321c6cf
Brandan Darby <brandantd@gmail.com>
2019-03-26 06:06:10 -0500

    Added Healthbar and UI integration

---
+++ GameArt/Assets/Scripts/Game Manager/SaveCheckpoint/Checkpoint.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Checkpoint : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Renderer rend = GetComponent<Renderer>();
+        GameObject checkpointManager = GameObject.Find("CheckpointManager");
+        if (other.tag == "Player" && checkpointManager != null)
+        {
+            checkpointManager.GetComponent<CheckpointManager>().checkpointLocation = transform.position;
+            checkpointManager.GetComponent<CheckpointManager>().checkpointScene = SceneManager.GetActiveScene();
+            checkpointManager.GetComponent<CheckpointManager>().checkpointSaved = true;
+            rend.material.SetColor("_Color", Color.yellow);
+        }
+    }
+}
---
+++ GameArt/Assets/Scripts/Projectile/grappleHook.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grappleHook : MonoBehaviour
+{
+    public float timer;
+    public float speed = 10;
+    private bool contact = false;
+    public bool shootRight;
+    private Vector3 velocity;
+    public Rigidbody rb;
+    private PlayerController player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0 && !contact)
+        {
+            Destroy(gameObject);
+        }
+        if (GameObject.Find("Player(Cube)").transform.localScale.x > 0) shootRight = true;
+        else shootRight = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 pos = transform.position;
+        if (shootRight)
+        {
+            velocity = new Vector3(speed * Time.deltaTime, speed * Time.deltaTime, 0);
+        }
+        else
+        {
+            velocity = new Vector3(-speed * Time.deltaTime, speed * Time.deltaTime, 0);
+        }
+        pos += transform.rotation * velocity;
+        transform.position = pos;
+
+        timer -= Time.deltaTime;
+
+        if (timer <= 0 && !contact)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Grapple Surface"))
+        {
+            contact = true;
+            speed = 0;
+            GameObject.Find("Player(Cube)").GetComponent<PlayerController>().grappleConnection = true;
+            //Destroy(gameObject, .5f);
+        }
+        else if (other.CompareTag("Player"))
+        {
+            //blank space, will change later
+        }
+        else if (!other.CompareTag("Grapple Surface"))
+        {
+            //play 'dink' sound
+            contact = false;
+            Destroy(gameObject);
+        }
+    }
+}
