3974e247aad80683cce3f31ea93d81d4434fb088
CampRF21 <rhcampos_lt@hotmail.com>
2019-04-02 01:37:45 -0500

    added basic main menu

---
+++ GameArt/Assets/mainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+    public void playGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void quitGame()
+    {
+        Debug.Log("Quit program");
+        Application.Quit();
+    }
+}
